a1ea3dfd73300dc1290d8a0b3d5d4430d3dc0cf1
sahilsachdevaa <sahilsachdevaa@hotmail.com>
2018-03-17 11:31:51 +0530

    Gst Invoice
    merge chaNGES

--- GSTINVOICE/LoginForm.cs
+++ GSTINVOICE/LoginForm.cs
@@ -47,10 +47,13 @@ namespace GSTINVOICE
                     if (count == 1)
                     {
                         this.isloginsuccess = true;
+<<<<<<< HEAD
 
 
                         // Application.Run(new MDIContainer());
 
+=======
+>>>>>>> 67f327f8e5a88c4f1f2f5c601d56419f05787891
                         this.Hide();
                         this.container.EnableControls();
                     }
